ffe79a7315cf3516b143c672ed55a1671eda52ab
TijnAltenaAO <64588832+TijnAltenaAO@users.noreply.github.com>
2020-04-30 06:24:16 -0700

    Add files via upload

---
+++ PremieBerekenenTijnAltena/PremieBerekenenAltena.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PremieBerekenenTijnAltena
+{
+    partial class test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Noord",
+            "Midden",
+            "Zuid"});
+            this.comboBox2.Location = new System.Drawing.Point(102, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            this.comboBox2.MouseEnter += new System.EventHandler(this.ComboBox2_MouseEnter);
+            this.comboBox2.MouseLeave += new System.EventHandler(this.ComboBox2_MouseLeave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Premie berekenen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Regio:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "De waarde van het eigendom";
+            this.textBox2.Click += new System.EventHandler(this.TextBox2_Click);
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Verzekering:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-1, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Eigendomswaarde:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = " Korting (%) :";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(102, 105);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(102, 17);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Autoverzekering";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(102, 141);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 17);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.Text = "Huisverzekering";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(102, 177);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(121, 17);
+            this.radioButton3.TabIndex = 18;
+            this.radioButton3.Text = "Inboedelverzekering";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(365, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 186);
+            this.listBox1.TabIndex = 19;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(235, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(108, 56);
+            this.richTextBox1.TabIndex = 20;
+            this.richTextBox1.Text = "Noord: 25% korting\nMidden: 10% korting\nZuid: 15% korting";
+            this.richTextBox1.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 255);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "test";
+            this.Text = "Premie Berekenen Altena";
+            this.Load += new System.EventHandler(this.Test_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ PremieBerekenenTijnAltena/PremieBerekenenAltena.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PremieBerekenenTijnAltena
+{
+    public partial class test : Form
+    {
+        public test()
+        {
+            InitializeComponent();
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void ComboBox2_MouseEnter(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = true;
+        }
+
+        private void Test_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox2_MouseLeave(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = false;
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 0:
+                    textBox1.Text = "25";
+                    break;
+                case 1:
+                    textBox1.Text = "10";
+                    break;
+                case 2:
+                    textBox1.Text = "15";
+                    break;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == 0)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    listBox1.Items.Add(100 + (Convert.ToInt32(textBox2.Text) * 0.03)).ToString();
+                    listBox1.Items.Add((100 + (Convert.ToInt32(textBox2.Text) * 0.03)) * 0.75).ToString();
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    listBox1.Items.Add(150 + (Convert.ToInt32(textBox2.Text) * 0.01)).ToString();
+                    listBox1.Items.Add((150 + (Convert.ToInt32(textBox2.Text) * 0.01)) * 0.75).ToString();
+                }
+                else
+                {
+                    listBox1.Items.Add(100 + (Convert.ToInt32(textBox2.Text) * 0.001)).ToString();
+                    listBox1.Items.Add((100 + (Convert.ToInt32(textBox2.Text) * 0.001)) * 0.75).ToString();
+                }
+
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    listBox1.Items.Add(100 + (Convert.ToInt32(textBox2.Text) * 0.03)).ToString();
+                    listBox1.Items.Add((100 + (Convert.ToInt32(textBox2.Text) * 0.03)) * 0.90).ToString();
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    listBox1.Items.Add(150 + (Convert.ToInt32(textBox2.Text) * 0.01)).ToString();
+                    listBox1.Items.Add((150 + (Convert.ToInt32(textBox2.Text) * 0.01)) * 0.90).ToString();
+                }
+                else
+                {
+                    listBox1.Items.Add(100 + (Convert.ToInt32(textBox2.Text) * 0.001)).ToString();
+                    listBox1.Items.Add((100 + (Convert.ToInt32(textBox2.Text) * 0.001)) * 0.90).ToString();
+                }
+
+            }
+            if (comboBox2.SelectedIndex == 2)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    listBox1.Items.Add(100 + (Convert.ToInt32(textBox2.Text) * 0.03)).ToString();
+                    listBox1.Items.Add((100 + (Convert.ToInt32(textBox2.Text) * 0.03)) * 0.85).ToString();
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    listBox1.Items.Add(150 + (Convert.ToInt32(textBox2.Text) * 0.01)).ToString();
+                    listBox1.Items.Add((150 + (Convert.ToInt32(textBox2.Text) * 0.01)) * 0.85).ToString();
+                }
+                else
+                {
+                    listBox1.Items.Add(100 + (Convert.ToInt32(textBox2.Text) * 0.001)).ToString();
+                    listBox1.Items.Add((100 + (Convert.ToInt32(textBox2.Text) * 0.001)) * 0.85).ToString();
+                }
+
+            }
+        }
+    }
+}
+            
